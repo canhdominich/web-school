10f82ed759014807d075d4bd68050ed87acda665
Carnell <canhdd.hust.soict@gmail.com>
2025-09-06 23:00:49 +0700

    update code

--- webapp/src/components/term/TermDataTable.tsx
+++ webapp/src/components/term/TermDataTable.tsx
@@ -341,7 +341,7 @@ export default function TermDataTable({
       type="button"
       className="btn btn-success btn-update-event flex justify-center rounded-lg bg-brand-500 px-4 py-2.5 text-sm font-medium text-white hover:bg-brand-600"
     >
-      Thêm tiến độ
+      Thêm sự kiện
     </button>
   );
 
